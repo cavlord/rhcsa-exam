f97f2f1862e6acf51596c0e3169e781d9638a59f
cavlord <46516339+cavlord@users.noreply.github.com>
2026-05-18 04:44:40 +0700

    Update setup.sh

--- ubuntu-simple/step1/setup.sh
+++ ubuntu-simple/step1/setup.sh
@@ -11,3 +11,11 @@ cd /root
 ./setup_lab_ubuntu.sh
 
 echo "Lab environment ready!"
+echo ""
+echo "Starting RHCSA Exam Menu..."
+sleep 2
+
+# Launch exam menu
+./rhcsa_exam_menu.sh
+
+# Made with Bob
