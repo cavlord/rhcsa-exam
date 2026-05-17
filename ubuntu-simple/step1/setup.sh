a3dd643de80da5eb8bbae6dd82cfb0e0605399b6
cavlord <46516339+cavlord@users.noreply.github.com>
2026-05-18 03:51:47 +0700

    Add files via upload

---
+++ ubuntu-simple/step1/setup.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+# Wait for system to be ready
+sleep 2
+
+# Make scripts executable
+chmod +x /root/*.sh
+
+# Run the lab setup
+cd /root
+./setup_lab.sh
+
+echo "Lab environment ready!"
